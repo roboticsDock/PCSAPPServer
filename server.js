29a210f58aa7ce09a9dd42bc853fc3516eb88352
roboticsDock <roboticsdock@gmail.com>
2022-02-08 22:24:06 +0530

    Commit- Added Cors

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 //const router = express.Router();
-const cors = require("cors");
+const cors = require('cors');
 app.use(cors());
 
 app.use(express.json());
@@ -34,6 +34,12 @@ const conn = {
 
 
 app.use(bodyParser.urlencoded({extended:true}))
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 //const db = mssql.connect(config);
 
