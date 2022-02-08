4c71605cab044f999662136a850c45997a2f2355
roboticsDock <roboticsdock@gmail.com>
2022-02-08 23:39:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -35,25 +35,11 @@ const conn = {
 
 app.use(bodyParser.urlencoded({extended:true}))
 
-// Add headers
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-});
+  });
 
 //const db = mssql.connect(config);
 
@@ -163,12 +149,8 @@ app.post("/api/InsertInv",(req, res) => {
     const db = mssql.connect(conn).then(pool => {
         return pool.request()
         .query(sqlInsertRows, (err,result) => { 
-            res.setHeader('Access-Control-Allow-Origin', '*');
-            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-            res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
-            res.setHeader('Access-Control-Allow-Credentials', true); // If needed
-                    
             res.send(result);
+            return res;
         })   
     })
 })
